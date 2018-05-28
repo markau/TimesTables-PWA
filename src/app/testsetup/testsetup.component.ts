248d33832fbe2e3b5de1d75ce73f54c462d25b01
Mark Andrews <20metresbelow@gmail.com>
2018-05-28 16:22:55 +0930

    included add to homescreen promopt

--- src/app/testsetup/testsetup.component.ts
+++ src/app/testsetup/testsetup.component.ts
@@ -24,27 +24,27 @@ export class TestsetupComponent implements OnInit {
 
   ngOnInit() {
 
-    // if (window.matchMedia("(display-mode: browser").matches) {
-    //   // We are in the browser
-    //   window.addEventListener("beforeinstallprompt", event => {
-    //     event.preventDefault();
-    //     const sb = this.snackBar.open(
-    //       "Do you want to install this App?",
-    //       "Install",
-    //       { duration: 5000 }
-    //     );
-    //     sb.onAction().subscribe(() => {
-    //       (event as any).prompt();
-    //       (event as any).userChoice.then(result => {
-    //         if (result.outcome === "dismissed") {
-    //           // TODO: Track no installation
-    //         } else {
-    //           // TODO: It was installed
-    //         }
-    //       });
-    //     });
-    //     return false;
-    //   });
-    // }
+    if (window.matchMedia("(display-mode: browser").matches) {
+      // We are in the browser
+      window.addEventListener("beforeinstallprompt", event => {
+        event.preventDefault();
+        const sb = this.snackBar.open(
+          "Do you want to install this App?",
+          "Install",
+          { duration: 5000 }
+        );
+        sb.onAction().subscribe(() => {
+          (event as any).prompt();
+          (event as any).userChoice.then(result => {
+            if (result.outcome === "dismissed") {
+              // TODO: Track no installation
+            } else {
+              // TODO: It was installed
+            }
+          });
+        });
+        return false;
+      });
+    }
   }
 }
