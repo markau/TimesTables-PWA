ed554c2cd5ac88c056469daddbdd96359f5b05d6
Mark Andrews <20metresbelow@gmail.com>
2018-06-04 12:56:34 +0930

    formatted html

--- src/app/aboutpage/aboutpage.component.ts
+++ src/app/aboutpage/aboutpage.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-aboutpage',
-  templateUrl: './aboutpage.component.html',
-  styleUrls: ['./aboutpage.component.scss']
+  selector: "app-aboutpage",
+  templateUrl: "./aboutpage.component.html",
+  styleUrls: ["./aboutpage.component.scss"]
 })
 export class AboutpageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
