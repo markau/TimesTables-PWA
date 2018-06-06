f5d438e18b1dffb952fb7cab2c9a7502c2b79df4
Mark Andrews <20metresbelow@gmail.com>
2018-06-06 13:15:20 +0930

    more tests

--- src/app/resultpage/resultpage.component.spec.ts
+++ src/app/resultpage/resultpage.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
 
 import { MinuteSecondsPipe } from "../minute-seconds.pipe";
 import { DataService } from "../data.service";
@@ -23,6 +25,9 @@ const appRoutes: Array<Object> = [
 describe("ResultpageComponent", () => {
   let component: ResultpageComponent;
   let fixture: ComponentFixture<ResultpageComponent>;
+  let debugElement: DebugElement;
+  let dataService: DataService;
+  let saveToLocalStorageSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -45,10 +50,25 @@ describe("ResultpageComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ResultpageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    dataService = debugElement.injector.get(DataService);
+    saveToLocalStorageSpy = spyOn(component, "saveToLocalStorage").and.callThrough();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("saveToLocalStorage() is called on the component", async(() => {
+    component.dataService.testState.isTestComplete = true;
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(saveToLocalStorageSpy).toHaveBeenCalled();
+    });
+    component.ngOnInit();
+  }));
+
+
 });
--- src/app/testsetup/testsetup.component.spec.ts
+++ src/app/testsetup/testsetup.component.spec.ts
@@ -3,6 +3,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { By } from "@angular/platform-browser";
 import { DebugElement } from "@angular/core";
+// import { MatRadioModule } from "@angular/material/radio";
 
 import { TestsetupComponent } from "./testsetup.component";
 import { DataService } from "../data.service";
@@ -36,4 +37,16 @@ describe("TestsetupComponent", () => {
     expect(component).toBeTruthy();
   });
 
+  it("Button label via async() and whenStable()", async(() => {
+    const el = debugElement
+    .query(By.css(".radio-2"));
+    el.triggerEventHandler("click", 3);
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      // console.log(el.nativeElement);
+      // expect(radioClickSpy).toHaveBeenCalledWith(3);
+    });
+    component.ngOnInit();
+  }));
 });
