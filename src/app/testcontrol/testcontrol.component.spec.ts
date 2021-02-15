1218a70070dcfbd04b1c07aabcf1d92720d439a8
Mark Andrews <mark.andrews@unisa.edu.au>
2021-02-16 08:36:33 +1030

    Angular 11 core cli

--- src/app/testcontrol/testcontrol.component.spec.ts
+++ src/app/testcontrol/testcontrol.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { Location } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, ComponentRef } from "@angular/core";
 import { TestcontrolComponent } from "./testcontrol.component";
@@ -31,7 +31,7 @@ describe("TestcontrolComponent", () => {
   let dataService: DataService;
   let location: Location;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule.withRoutes(appRoutes)
